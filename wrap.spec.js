457c68e4670bb57f637fd04e16871d359df3fbe7
Lily Tan <lilyt93@gmail.com>
2019-11-21 11:57:23 -0500

    added more test spects

--- wrap.spec.js
+++ wrap.spec.js
@@ -2,16 +2,24 @@ const { expect } = require("chai");
 const wrap = require("./wrap")
 
 describe("wrap", () => {
+    beforeEach('Setting up wrap function', function() {
+        wrap('Lorem ipsum dolor sit eu amet, elit na magna sem amet nulla vel purus ac ligula.', 20)
+    })
+
     it("Returns empty string if empty string was provided", () => {
         expect(wrap("", 10)).to.equal("");
     });
 
     it("First parameter is a string", () => {
-        expect(wrap(typeof line)).to.equal("string"); 
+        expect(typeof line).to.be.an("string");
     });
 
     it("Second parameter is a number", () => {
-        expect(maxLen).to.be.an("number")
+        expect(typeof maxLen).to.be.an("Number")
     });
+
+    // it("Line length is less than number", () => {
+    //     expect(line < maxLen)
+    // })
 });
 
